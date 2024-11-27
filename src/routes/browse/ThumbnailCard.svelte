0c76c6d1e0b6061bb2261993c1895bc4c5893d68
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-11-27 10:50:01 +0700

    rename video icons

--- src/routes/browse/ThumbnailCard.svelte
+++ src/routes/browse/ThumbnailCard.svelte
@@ -49,7 +49,7 @@
 				{
 					case "image": return `/browse/thumb?path=${name}`;
 					case "audio": return AUDIO_IMAGE_SOURCE;
-					case "video" : return MEDIA_IMAGE_SOURCE
+					case "video" : return VIDEO_IMAGE_SOURCE
 				}
 
 				return "";
@@ -72,7 +72,7 @@
 					{
 						case "image": return IMAGE_ICON_COLOR;
 						case "audio": return AUDIO_ICON_COLOR;
-						case "video" : return MEDIA_ICON_COLOR
+						case "video" : return VIDEO_ICON_COLOR
 					}
 
 				return "";
@@ -89,7 +89,7 @@
 
 	const DIRECTORY_ICON_COLOR = 'goldenrod';
 	const ZIP_ICON_COLOR = 'yellowgreen';
-	const MEDIA_ICON_COLOR = 'teal';
+	const VIDEO_ICON_COLOR = 'teal';
 	const IMAGE_ICON_COLOR = 'lightblue';
 	const AUDIO_ICON_COLOR = 'yellow';
 
@@ -106,9 +106,9 @@
 			<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm5.5-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9v1H8v1h1v1H8v1h1v1H7.5V5h-1V4h1V3h-1V2h1z"/>
 		</svg>`;
 
-	const MEDIA_IMAGE_SOURCE = `
+	const VIDEO_IMAGE_SOURCE = `
 		data:image/svg+xml;utf8,
-		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="16" fill="${MEDIA_ICON_COLOR}" class="bi bi-file-play" viewBox="0 0 16 16">
+		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="16" fill="${VIDEO_ICON_COLOR}" class="bi bi-file-play" viewBox="0 0 16 16">
 			<path d="M6 10.117V5.883a.5.5 0 0 1 .757-.429l3.528 2.117a.5.5 0 0 1 0 .858l-3.528 2.117a.5.5 0 0 1-.757-.43z"/>
 			<path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1"/>
 		</svg>`;
