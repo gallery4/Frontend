c9eea94167c99b937a26478ad24e834b7fb65527
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-16 21:07:34 +0700

    modernize browse page.

---
+++ src/lib/components/SideBar.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	//import { browseURL, tagURL, historyURL, userURL, aboutURL } from '$lib/routes';
+
+	import { Icon } from 'svelte-icon';
+	import browseIcon from '@mdi/svg/svg/bookshelf.svg?raw';
+	import tagsIcon from '@mdi/svg/svg/tag-multiple.svg?raw';
+	import historyIcon from '@mdi/svg/svg/history.svg?raw';
+	import userIcon from '@mdi/svg/svg/account.svg?raw';
+	import aboutIcon from '@mdi/svg/svg/information.svg?raw';
+
+	let { children = undefined, showMenu = $bindable() } = $props();
+</script>
+
+<div class="drawer-side">
+	<div
+		class="drawer-overlay"
+		role="none"
+		onkeydown={() => (showMenu = false)}
+		onclick={() => (showMenu = false)}
+	></div>
+	<ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+		{@render children?.()}
+
+		<li class="menu-title">Navigation</li>
+
+		<!-- ul class="list">
+			<li class="list-row">
+				<button onclick={() => goto(browseURL(page.url.origin))}>
+					<Icon data={browseIcon} />&nbsp;Browse items
+				</button>
+			</li>
+			<li class="list-row">
+				<button onclick={() => goto(tagURL(page.url.origin))}>
+					<Icon data={tagsIcon} />&nbsp;Tag list
+				</button>
+			</li>
+
+			<li class="list-row">
+				<button onclick={() => goto(historyURL(page.url.origin))}>
+					<Icon data={historyIcon} />&nbsp;History
+				</button>
+			</li>
+			<li class="list-row">
+				<button onclick={() => goto(userURL(page.url.origin))}>
+					<Icon data={userIcon} />User
+				</button>
+			</li>
+			<li class="list-row">
+				<button onclick={() => goto(aboutURL(page.url.origin))}>
+					<Icon data={aboutIcon} />About
+				</button>
+			</li>
+		</ul -->
+	</ul>
+</div>
--- src/routes/browse/[...path]/+page.svelte
+++ src/routes/browse/[...path]/+page.svelte
@@ -1,23 +1,4 @@
 <script lang="ts">
-	import {
-		Button,
-		Col,
-		Collapse,
-		Container,
-		Icon,
-		Input,
-		InputGroup,
-		InputGroupText,
-		Nav,
-		Navbar,
-		NavbarBrand,
-		NavbarToggler,
-		NavItem,
-		NavLink,
-		Row,
-		Spinner
-	} from '@sveltestrap/sveltestrap';
-
 	import { navigating, page } from '$app/stores';
 	import { persistBrowserLocal } from '@macfja/svelte-persistent-store';
 	import { writable } from 'svelte/store';
@@ -25,6 +6,10 @@
 	import GridItem from './GridItem.svelte';
 	import ListItem from './ListItem.svelte';
 	import { compareItems, extractSort } from '$lib/utils';
+	import Container from '$lib/components/Container.svelte';
+	import Content from '$lib/components/Content.svelte';
+	import NavBar from '$lib/components/NavBar.svelte';
+	import SideBar from '$lib/components/SideBar.svelte';
 
 	const { data } = $props();
 
@@ -41,7 +26,9 @@
 	});
 
 	const directories = $derived({
-		objects: data.response.directories.toSorted((a: any, b: any) => compareItems(a, b, sortBy, order))
+		objects: data.response.directories.toSorted((a: any, b: any) =>
+			compareItems(a, b, sortBy, order)
+		)
 	});
 
 	let isOpen = $state(false);
@@ -62,124 +49,56 @@
 			inline: 'nearest'
 		});
 	}
+
+	let showMenu = $state(false);
 </script>
 
 <svelte:head>
 	<title>Gallery - Browse: {data.response.path}</title>
 </svelte:head>
 
-<Container class="sticky-top text-bg-light" fluid>
-	<Navbar dark expand="lg" container="lg">
-		<NavbarBrand href="/">Gallery</NavbarBrand>
-		<NavbarToggler on:click={() => (isOpen = !isOpen)} />
-		<Collapse {isOpen} navbar expand="lg" on:update={handleUpdate}>
-			<Nav class="ms-auto mt-lg-0 mt-3 col-lg-5" navbar>
-				<Breadcrumb path={data.response.path}></Breadcrumb>
-			</Nav>
-			<Nav navbar class="ms-lg-1 mt-lg-0 mt-3">
-				<InputGroup>
-					<InputGroupText><Icon name="sort-down" /></InputGroupText>
-					<Input type="select" bind:value={$sort}>
-						<option value="name ascending">name ascending</option>
-						<option value="name descending">name descending</option>
-						<option value="dateTime ascending">date-time ascending</option>
-						<option value="dateTime descending">date-time descending</option>
-					</Input>
-
-					<Button active={$browseView == 'grid'} onclick={() => browseView.set('grid')}>
-						<Icon name="grid" />
-					</Button>
-					<Button active={$browseView == 'list'} onclick={() => browseView.set('list')}>
-						<Icon name="list" />
-					</Button>
-				</InputGroup>
-			</Nav>
-			<Nav class="ms-auto" navbar>
-				<NavItem>
-					<NavLink href="/about"><Icon name="info-circle"/>&nbsp;About</NavLink>
-				</NavItem>
-			</Nav>
-		</Collapse>
-	</Navbar>
-</Container>
-
-{#if $browseView == 'grid'}
-	<Container>
-		{#if $navigating}
-			<div use:moveToHash>
-				<Row cols={{ xl: 4, lg: 3, md: 2, sm: 1, xs: 1 }}>
-					{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as i}
-						<Col class="mt-3">
-							<GridItem name="loading_{i}" type="placeholder" />
-						</Col>
-					{/each}
-				</Row>
-			</div>
-		{:else}
-			<div use:moveToHash>
-				<Row cols={{ xl: 4, lg: 3, md: 2, sm: 1, xs: 1 }}>
+<Container bind:showMenu>
+	<Content>
+		<NavBar bind:showMenu title="Browse: {data.response.path}" />
+		<div class="prose container mx-auto mt-4 max-w-[1024px]">
+			{#if $browseView == 'grid'}
+				<div use:moveToHash class="grid grid-cols-1 gap-8 md:grid-cols-3">
 					{#if directories}
 						{#each directories.objects as object}
-							<Col class="mt-3">
-								<GridItem name={object.name} type="directory" />
-							</Col>
+							<GridItem name={object.name} type="directory" />
 						{/each}
 					{/if}
 					{#if archives}
 						{#each archives.objects as object}
-							<Col class="mt-3">
-								<GridItem name={object.name} type="zip" />
-							</Col>
+							<GridItem name={object.name} type="zip" />
 						{/each}
 					{/if}
 					{#if files}
 						{#each files.objects as object}
-							<Col class="mt-3">
-								<GridItem name={object.name} type="file" />
-							</Col>
+							<GridItem name={object.name} type="file" />
 						{/each}
 					{/if}
-				</Row>
-			</div>
-		{/if}
-	</Container>
-{/if}
-
-{#if $browseView == 'list'}
-	<Container>
-		{#if $navigating}
-			<div use:moveToHash>
-				{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as i}
-					<ListItem name="loading_{i}" type="placeholder" dateTime={new Date()} />
-				{/each}
-			</div>
-		{:else}
-			<div use:moveToHash>
-				{#if directories}
-					{#each directories.objects as object}
-						<ListItem name={object.name} type="directory" dateTime={object.dateTime} />
-					{/each}
-				{/if}
-				{#if archives}
-					{#each archives.objects as object}
-						<ListItem name={object.name} type="zip" dateTime={object.dateTime} />
-					{/each}
-				{/if}
-				{#if files}
-					{#each files.objects as object}
-						<ListItem name={object.name} type="file" dateTime={object.dateTime} />
-					{/each}
-				{/if}
-			</div>
-		{/if}
-	</Container>
-{/if}
-
-{#if $navigating}
-	<div
-		class="position-absolute top-50 start-50 translate-middle justify-content-center"
-		style="background-color:#00000080; padding: 1em;"
-	>
-		<span> <Spinner type="border"></Spinner>&nbsp;Loading</span>
-	</div>
-{/if}
+				</div>
+			{:else if $browseView == 'list'}
+				<div use:moveToHash>
+					{#if directories}
+						{#each directories.objects as object}
+							<ListItem name={object.name} type="directory" dateTime={object.dateTime} />
+						{/each}
+					{/if}
+					{#if archives}
+						{#each archives.objects as object}
+							<ListItem name={object.name} type="zip" dateTime={object.dateTime} />
+						{/each}
+					{/if}
+					{#if files}
+						{#each files.objects as object}
+							<ListItem name={object.name} type="file" dateTime={object.dateTime} />
+						{/each}
+					{/if}
+				</div>
+			{/if}
+		</div>
+	</Content>
+	<SideBar bind:showMenu></SideBar>
+</Container>
