e4e3d44a7daddc74b896bd80ecf0674cd671d8af
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-17 07:08:53 +0700

    update ListItem

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,3 +1,12 @@
+import imageIcon from '@mdi/svg/svg/image.svg?raw';
+import audioIcon from '@mdi/svg/svg/music-box.svg?raw'
+import videoIcon from '@mdi/svg/svg/movie.svg?raw'
+import pdfIcon from '@mdi/svg/svg/file-pdf-box.svg?raw'
+import epubIcon from '@mdi/svg/svg/book.svg?raw'
+import directoryIcon from '@mdi/svg/svg/folder.svg?raw'
+import zipIcon from '@mdi/svg/svg/folder-zip.svg?raw'
+import placeholderIcon from '@mdi/svg/svg/square-rounded.svg?raw'
+
 export function getIconColor(type: string, filetype: string | false | undefined): string {
     switch (type) {
         case 'file':
@@ -29,12 +38,43 @@ export function getIconColor(type: string, filetype: string | false | undefined)
     return '';
 }
 
+export function getIconClass(type: string, filetype: string | false | undefined): string {
+    switch (type) {
+        case 'file':
+            switch (filetype) {
+                case 'image':
+                    return 'fill-sky-500 stroke-sky-900';
+                case 'audio':
+                    return 'fill-yellow-500 stroke-yellow-900';
+                case 'video':
+                    return 'fill-teal-500 stroke-teal-900';
+                case 'pdf':
+                    return 'fill-red-500 stroke-red-900';
+                case 'epub':
+                    return 'fill-lime-500 stroke-lime-900';
+            }
+
+            return '';
+
+        case 'directory':
+            return 'fill-amber-500 stroke-amber-900';
+
+        case 'zip':
+            return 'fill-emerald-500 stroke-emerald-900';
+
+        case 'placeholder':
+            return 'fill-gray-500 stroke-gray-900';
+    }
+
+    return '';
+}
+
 export function getIconImage(type: string, filetype: string | false | undefined): string {
     switch (type) {
         case 'file':
             switch (filetype) {
                 case 'image':
-                    return ""; 
+                    return "";
                 case 'audio':
                     return AUDIO_IMAGE_SOURCE;
                 case 'video':
@@ -90,6 +130,38 @@ export function getIconName(type: string, filetype: string | false | undefined):
 
     return '';
 }
+
+export function getIcon(type: string, filetype: string | false | undefined): string {
+    switch (type) {
+        case 'file':
+            switch (filetype) {
+                case 'image':
+                    return imageIcon;
+                case 'audio':
+                    return audioIcon;
+                case 'video':
+                    return videoIcon;
+                case 'pdf':
+                    return pdfIcon;
+                case 'epub':
+                    return epubIcon;
+            }
+
+            return '';
+
+        case 'directory':
+            return directoryIcon;
+
+        case 'zip':
+            return zipIcon;
+
+        case 'placeholder':
+            return placeholderIcon;
+    }
+
+    return '';
+}
+
 
 const DIRECTORY_ICON_COLOR = 'goldenrod';
 const ZIP_ICON_COLOR = 'yellowgreen';
--- src/routes/browse/[...path]/ListItem.svelte
+++ src/routes/browse/[...path]/ListItem.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { getIconColor, getIconImage, getIconName } from '$lib/icons';
+	import { getIcon, getIconClass } from '$lib/icons';
 	import { createElementId, determinFileType, getFilenameFromKey } from '$lib/utils';
+	import { Icon } from 'svelte-icon';
 	import 'vidstack/bundle';
 
 	let { name, type, dateTime } = $props();
@@ -18,7 +19,14 @@
 		return url.toString();
 	});
 
-	function getLink(): string {
+	const mediaUrl = $derived.by(() => {
+		let url = new URL('/api/get', page.url.origin);
+		url.searchParams.set('path', name);
+
+		return url.toString();
+	});
+
+	const linkUrl = $derived.by(() => {
 		switch (type) {
 			case 'file':
 				return `/view/${name}`;
@@ -29,93 +37,86 @@
 		}
 
 		return '';
-	}
+	});
 
-	function getImageSource(): string {
-		if (type != 'file') return getIconImage(type, false);
+	const thumbnailUrl = $derived.by(() => {
+		let url = new URL('/api/thumbnail', page.url.origin);
+		url.searchParams.set('path', name);
+		url.searchParams.set('type', 'LIST');
+		return url.toString();
+	});
 
-		const filetype = determinFileType(name);
-		if (filetype == 'image') {
-			let url = new URL('/api/thumbnail', page.url.origin);
-			url.searchParams.set('path', name);
-			url.searchParams.set('type', 'LIST');
-			return url.toString();
-		}
-
-		return getIconImage(type, filetype);
-	}
+	const filename = $derived.by(() => getFilenameFromKey(name, type));
 </script>
 
-<div id={createElementId(getFilenameFromKey(name, type))} class="h-30 m-4 flex shadow">
+<div id={createElementId(filename)} class="h-30 m-4 flex shadow">
 	<div class="m-2 my-auto w-16 flex-none">
 		{#if type == 'file'}
 			{#if filetype == 'video'}
-				<media-player title={getFilenameFromKey(name, 'media')} src="/get/file/{name}">
+				<media-player title={filename} src="/get/file/{name}">
 					<media-provider></media-provider>
 					<media-video-layout></media-video-layout>
 				</media-player>
 			{:else if filetype == 'audio'}
-				<img alt="thumbnail" style="height: 100px;" loading="lazy" src={getImageSource()} />
+				<Icon
+					class={getIconClass(type, filetype)}
+					data={getIcon(type, filetype)}
+					width="64px"
+					height="64px"
+				/>
 			{:else if filetype == 'image'}
 				{#if !loaded}
-					<span
-						class="loading loading-dots loading-sm"
-						style="color:{getIconColor(type, filetype)};"
-					></span>
+					<span class="loading loading-dots my-auto"></span>
 				{/if}
 				<div class="tooltip tooltip-right">
 					<div class="tooltip-content">
-						<img class="object-cover" src={hoverThumbImage} alt="hover" />
+						<img src={hoverThumbImage} alt="hover" />
 					</div>
 
 					<img
 						alt="thumbnail"
 						class="thumbnail"
 						loading="lazy"
-						src={getImageSource()}
+						src={thumbnailUrl}
 						onload={() => {
 							loaded = true;
 						}}
 					/>
 				</div>
 			{:else if filetype == 'pdf'}
-				<img alt="thumbnail" style="height: 100px;" loading="lazy" src={getImageSource()} />
+				<Icon
+					data={getIcon(type, filetype)}
+					width="64px"
+					height="64px"
+					class={getIconClass(type, filetype)}
+				/>
 			{/if}
 		{:else}
-			<img
-				alt="thumbnail"
-				style="height: 100px;"
-				loading="lazy"
-				src={getImageSource()}
-				onload={() => {
-					loaded = true;
-				}}
+			<Icon
+				data={getIcon(type, filetype)}
+				width="64px"
+				height="64px"
+				class={getIconClass(type, filetype)}
 			/>
 		{/if}
 	</div>
 	<div class="m-1 my-2 w-64 flex-1">
-		<a href={getLink()}>
+		<a href={linkUrl}>
 			{#if type == 'placeholder'}
 				<span class="placeholder"></span>
 			{:else}
-				<!--Icon name={getIconName(type, filetype)} style="color:{getIconColor(type, filetype)};"
-				></Icon -->
-				&nbsp;{getFilenameFromKey(name, type)}
+				{filename}
 			{/if}
 		</a>
 
 		{#if type == 'file' && filetype == 'audio'}
-			<media-player
-				class="d-block"
-				title={getFilenameFromKey(name, 'media')}
-				src="/get/file/{name}"
-			>
+			<media-player class="d-block" title={filename} src={mediaUrl}>
 				<media-provider></media-provider>
 				<media-audio-layout></media-audio-layout>
 			</media-player>
 		{/if}
 	</div>
-	<div class="w-24 my-2 flex-none">
+	<div class="my-2 w-24 flex-none">
 		{new Date(dateTime).toLocaleString()}
 	</div>
 </div>
