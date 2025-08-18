df1a6f0d5986027531fb83d8883a9db34cf288ca
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-19 04:34:07 +0700

    add close button on view epub page.

--- src/routes/view/epub/[...path]/+page.svelte
+++ src/routes/view/epub/[...path]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Icon } from 'svelte-icon';
 	import downloadIcon from '@mdi/svg/svg/download.svg?raw';
+	import closeIcon from '@mdi/svg/svg/close.svg?raw';
 
 	import Container from '$lib/components/Container.svelte';
 	import Content from '$lib/components/Content.svelte';
@@ -8,6 +9,9 @@
 	import SideBar from '$lib/components/SideBar.svelte';
 	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
 	import SvelteReader from 'svelte-reader';
+	import { createBrowseURL } from '$lib/navigation.js';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
 
 	const { data } = $props();
 
@@ -21,7 +25,7 @@
 <Container bind:showMenu>
 	<Content>
 		<NavBar bind:showMenu title={data.filename} />
-		<div class="fixed bottom-0 end-0 start-0 top-20 h-full w-full">
+		<div class="fixed bottom-0 end-0 start-0 top-20 flex h-full w-full justify-center">
 			<SvelteReader
 				url={data.mediaURL}
 				title="&nbsp;"
@@ -29,6 +33,13 @@
 				tocChanged={() => {}}
 			/>
 		</div>
+
+		<button
+			class="fixed end-2 top-20 z-10 h-20 w-20 cursor-pointer text-gray-500/50 hover:text-gray-500"
+			onclick={() => goto(createBrowseURL(data.parent, page.url.origin, data.filename))}
+		>
+			<Icon data={closeIcon} class="mx-auto"></Icon>
+		</button>
 	</Content>
 	<SideBar bind:showMenu>
 		<ul class="menu">
