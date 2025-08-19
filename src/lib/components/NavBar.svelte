dd3d536e6d9c35c4fb427f8c5c89e052e28c40ab
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-19 23:29:34 +0700

    hide browser control on non-pwa

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -5,22 +5,26 @@
 	import refreshIcon from '@mdi/svg/svg/refresh.svg?raw';
 	import logo from '$lib/logo.svg?raw';
 	import { page } from '$app/state';
-	
+
+	import { pwaInfo } from 'virtual:pwa-info';
+
 	let { title, showMenu = $bindable(), hasmenu = true } = $props();
 </script>
 
 <div class="navbar bg-base-100 z-1 sticky top-0 h-16 shadow-sm">
 	<div class="mx-auto flex h-16 w-full max-w-[1024px]">
-		<div class="flex-none">
-			<button class="btn btn-square btn-ghost" onclick={() => window.history.back()}>
-				<Icon data={backIcon} />
-			</button>
-		</div>
-		<div class="flex-none">
-			<button class="btn btn-square btn-ghost" onclick={() => window.location.reload()}>
-				<Icon data={refreshIcon} />
-			</button>
-		</div>
+		{#if pwaInfo}
+			<div class="flex-none">
+				<button class="btn btn-square btn-ghost" onclick={() => window.history.back()}>
+					<Icon data={backIcon} />
+				</button>
+			</div>
+			<div class="flex-none">
+				<button class="btn btn-square btn-ghost" onclick={() => window.location.reload()}>
+					<Icon data={refreshIcon} />
+				</button>
+			</div>
+		{/if}
 		<div class="flex-none">
 			<a href={new URL('/browse', page.url.origin).toString()}>
 				<Icon data={logo} width="128px" height="48px" />
