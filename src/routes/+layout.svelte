7265940803934f132db6581ce482d3dfa1e4c090
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-19 21:46:30 +0700

    add pwa-info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,41 @@
 <script lang="ts">
 	import '../app.css';
+	import { pwaInfo } from 'virtual:pwa-info'; 
+	import { pwaAssetsHead } from 'virtual:pwa-assets/head';
+	import { onMount } from 'svelte';
+
+	let { children } = $props();
+  	let webManifestLink = $derived( pwaInfo ? pwaInfo.webManifest.linkTag : '')
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					//uncomment following code if you want check for updates
+					r && setInterval(() => {
+					   console.log('Checking for sw update')
+					   r.update()
+					}, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
 </script>
 
-<slot />
+<svelte:head> 
+ 	{@html webManifestLink} 
+	{#if pwaAssetsHead.themeColor}
+		<meta name="theme-color" content={pwaAssetsHead.themeColor.content} />
+	{/if}
+	{#each pwaAssetsHead.links as link}
+		<link {...link} />
+	{/each}
+</svelte:head>
+
+{@render children?.()}
--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,56 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { vite as vidstack } from 'vidstack/plugins';
 import { defineConfig, loadEnv } from 'vite';
+import { SvelteKitPWA} from '@vite-pwa/sveltekit'
 
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 
 	return {
-		plugins: [tailwindcss(), vidstack(), sveltekit(), devtoolsJson()],
+		plugins: [tailwindcss(), vidstack(), sveltekit(), SvelteKitPWA(
+			{
+				srcDir: './src',
+				mode: 'development',
+				// you don't need to do this if you're using generateSW strategy in your app
+				//strategies: generateSW ? 'generateSW' : 'injectManifest',
+
+				// you don't need to do this if you're using generateSW strategy in your app
+				//filename: generateSW ? undefined : 'prompt-sw.ts',
+				scope: '/',
+				base: '/',
+				selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+				pwaAssets: {
+					config: true,
+				},
+				manifest: {
+					short_name: 'Gallery 4',
+					name: 'Gallery 4',
+					start_url: '/',
+					scope: '/',
+					display: 'standalone',
+					theme_color: '#ffffff',
+					background_color: '#000000',
+				},
+				injectManifest: {
+					globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+				},
+				workbox: {
+					globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+				},
+				devOptions: {
+					enabled: false,
+					suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+					type: 'module',
+					navigateFallback: '/',
+				},
+				// if you have shared info in svelte config file put in a separate module and use it also here
+				kit: {
+					includeVersionFile: true,
+				},
+			}
+		), devtoolsJson()],
 		resolve: {
 			alias: [{ find: 'path', replacement: 'path-browserify' }]
 		},
-
-		// TODO: pdflib's service worker causes vidstack to work erraticly.
-		// this is temporary disable the error of missing DOMMatrix.
-		server:{
-			hmr:{
-				overlay: false
-			}
-		}
 	};
 });
